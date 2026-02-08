da44666c5af7bc875081cb9549bef2a49599f305
wdlin233 <wdlin233@163.com>
2026-02-08 22:05:27 +0800

    fix: utilize gcc and objcopy to bin files

--- kernel/build.rs
+++ kernel/build.rs
@@ -39,11 +39,12 @@ _num_app:
                 return None;
             }
             let name = entry.file_name().into_string().unwrap();
-            if path.extension().and_then(|s| s.to_str()) == Some("bin") {
-                Some(name)
-            } else {
-                None
+            if let Some(ext) = path.extension() {
+                if ext == "asm" {
+                    return None;
+                }
             }
+            Some(name)
         })
         .collect();
 
@@ -63,7 +64,9 @@ _num_app:
     for i in 0..apps.len() {
         writeln!(f, r#"    .quad app_{}_start"#, i)?;
     }
-    writeln!(f, r#"    .quad app_{}_end"#, apps.len() - 1)?;
+    if !apps.is_empty() {
+        writeln!(f, r#"    .quad app_{}_end"#, apps.len() - 1)?;
+    }
 
     for (idx, app) in apps.iter().enumerate() {
         println!("app_{}: {}", idx, app);
